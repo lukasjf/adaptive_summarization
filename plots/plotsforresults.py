a2956a6673d5ce35e6fa5b8520bf6f5a275cc524
Lukas <lukas.j.faber@gmail.com>
2018-04-16 10:26:37 -0400

    refactor plotting code, dotted line for test sets

--- plots/plotsforresults.py
+++ plots/plotsforresults.py
@@ -17,54 +17,42 @@ for row in data.iterrows():
     edgelimit = row[1]["EdgeThreshold"]
     break
 
+def read_method(method, parsed):
+    global base
+    data = pd.read_csv("results_" + base + "_" + method + ".csv", sep=",").fillna('')
+    for row in data.iterrows():
+        parsed[method]["objective"] += [row[1]["Objective"] / 50]
+        parsed[method]["sedges"] += [row[1]["SummaryEdges"]]
+        parsed[method]["snodes"] += [row[1]["SummaryNodes"]]
+        parsed[method]["test"] += [row[1]["TestObjective"] / 20]
 
-data = pd.read_csv("results_" + base + "_" + "existential.csv", sep=",").fillna('')
-for row in data.iterrows():
-    parsed["existential"]["objective"] += [row[1]["Objective"]]
-    parsed["existential"]["sedges"] += [row[1]["SummaryEdges"]]
-    parsed["existential"]["snodes"] += [row[1]["SummaryNodes"]]
-
-data = pd.read_csv("results_" + base + "_" + "variance.csv", sep=",").fillna('')
-for row in data.iterrows():
-    parsed["variance"]["objective"] += [row[1]["Objective"]]
-    parsed["variance"]["sedges"] += [row[1]["SummaryEdges"]]
-    parsed["variance"]["snodes"] += [row[1]["SummaryNodes"]]
-
-data = pd.read_csv("results_" + base + "_" + "combined.csv", sep=",").fillna('')
-for row in data.iterrows():
-    parsed["combined"]["objective"] += [row[1]["Objective"]]
-    parsed["combined"]["sedges"] += [row[1]["SummaryEdges"]]
-    parsed["combined"]["snodes"] += [row[1]["SummaryNodes"]]
+read_method("existential", parsed)
+read_method("variance", parsed)
+read_method("combined", parsed)
 
 
-for method in parsed:
-    plt.scatter(parsed[method]["snodes"], parsed[method]["objective"], label = method)
+c = {0: "#2ca02c", 1: "#1f77b4", 2: "#ff7f0e"}#, "3": "#d62728"}
+
+for i, method in enumerate(parsed):
+    plt.plot(parsed[method]["snodes"], parsed[method]["objective"], label = method, color = c[i])
+    plt.plot(parsed[method]["snodes"], parsed[method]["test"], label = method, linestyle="--", color = c[i])
 plt.legend(loc="upper right",fancybox=True)
 plt.xlabel("number of nodes in the summary")
 plt.ylabel("Objective function value")
-plt.axvline(x=nodelimit)
+#plt.axvline(x=nodelimit/2)
 plt.gca().set_ylim(bottom=0)
 plt.savefig(base + "nodesobj.pdf", dpi=600)
 plt.show()
 
-for method in parsed:
-    plt.scatter(parsed[method]["sedges"], parsed[method]["objective"], label = method)
+for i, method in enumerate(parsed):
+    plt.plot(parsed[method]["sedges"], parsed[method]["objective"], label = method, color = c[i])
+    plt.plot(parsed[method]["sedges"], parsed[method]["test"], label = method, linestyle="--", color=c[i])
 plt.legend(loc="upper right",fancybox=True)
 plt.xlabel("number of edges in the summary")
 plt.ylabel("Objective function value")
-plt.axvline(x=edgelimit)
+#plt.axvline(x=edgelimit/2)
 plt.gca().set_ylim(bottom=0)
 plt.savefig(base + "edgesobj.pdf", dpi=600)
-plt.show()
-
-for method in parsed:
-    plt.scatter(parsed[method]["snodes"], parsed[method]["sedges"], label = method)
-plt.legend(loc="upper right",fancybox=True)
-plt.xlabel("number of nodes in the summary")
-plt.ylabel("number of edges in the summary")
-plt.axvline(x=nodelimit)
-plt.axhline(y=edgelimit)
-plt.savefig(base + "nodesedges.pdf", dpi=600)
 plt.show()
 
 
