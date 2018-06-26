d312cbeb268f84740617b27615e3a37749bcdb0c
Lukas <lukas.j.faber@gmail.com>
2018-06-26 09:58:31 +0200

    add interface for merging summary merging strategies

--- src/main/java/summary/merging/MergedSummary.java
+++ src/main/java/summary/merging/MergedSummary.java
@@ -13,14 +13,16 @@ import java.util.*;
  */
 public class MergedSummary implements Benchmarkable {
 
-    private BaseGraph original;
-    private BaseGraph summary;
+    BaseGraph original;
+    BaseGraph summary;
     private int sizeLimit;
+    private MergeStrategy strategy;
 
-    public MergedSummary(BaseGraph originalGraph, int sizeLimit){
+    public MergedSummary(BaseGraph originalGraph, MergeStrategy strategy, int sizeLimit){
         this.original = originalGraph;
         this.summary = new BaseGraph();
         this.sizeLimit = sizeLimit;
+        this.strategy = strategy;
     }
 
     @Override
@@ -48,7 +50,7 @@ public class MergedSummary implements Benchmarkable {
 
             System.out.println(encoder.encode(summary));
 
-            int[] bestMerge = search(queries);
+            int[] bestMerge = strategy.search(this, queries);
             System.out.println(Arrays.toString(bestMerge));
             summary.removeNode(Integer.MAX_VALUE);
             int to = bestMerge[0];
@@ -69,68 +71,12 @@ public class MergedSummary implements Benchmarkable {
         return new SummaryEncoder().encode(summary);
     }
 
-    private double computeTrainingObjective(Map<BaseGraph, List<Map<String, String>>> queries) {
+    double computeTrainingObjective(Map<BaseGraph, List<Map<String, String>>> queries) {
         double objective = 0.0;
         for (BaseGraph query: queries.keySet()){
             List<Map<String, String>> summaryResults = new SubgraphIsomorphism().query(query, summary, false);
             objective += F1Score.fqScoreFor(queries.get(query), summaryResults);
         }
         return objective / queries.size();
-    }
-
-
-
-    private int[] search(Map<BaseGraph, List<Map<String, String>>> queries) {
-        int[] bestMerge = new int[]{Integer.MIN_VALUE, Integer.MIN_VALUE};
-        double bestObjective = Double.MIN_VALUE;
-        for (BaseNode n1: summary.getNodes()){
-            for (BaseNode n2: summary.getNodes()){
-                if (n2.getId() <= n1.getId() || n1.getId() == Integer.MAX_VALUE || n2.getId() == Integer.MAX_VALUE){
-                    continue;
-                }
-
-                Set<Integer> containedNodesBackup1 = new HashSet<>(n1.getContainedNodes());
-                Set<Integer> containedNodesBackup2 = new HashSet<>(n2.getContainedNodes());
-
-                summary.nodeWithId(Integer.MAX_VALUE).getContainedNodes().addAll(n1.getContainedNodes());
-                summary.nodeWithId(Integer.MAX_VALUE).getContainedNodes().addAll(n2.getContainedNodes());
-
-                for (BaseEdge e: summary.outEdgesFor(n1.getId())){
-                    summary.addEdge(Integer.MAX_VALUE, e.getTarget().getId(), e.getLabel());
-                }
-                for (BaseEdge e: summary.outEdgesFor(n2.getId())){
-                    summary.addEdge(Integer.MAX_VALUE, e.getTarget().getId(), e.getLabel());
-                }
-                for (BaseEdge e: summary.inEdgesFor(n1.getId())){
-                    summary.addEdge(e.getSource().getId(), Integer.MAX_VALUE, e.getLabel());
-                }
-                for (BaseEdge e: summary.inEdgesFor(n2.getId())){
-                    summary.addEdge(e.getSource().getId(), Integer.MAX_VALUE, e.getLabel());
-                }
-
-                n1.getContainedNodes().clear();
-                n2.getContainedNodes().clear();
-
-                double objective = computeTrainingObjective(queries);
-                if (objective > bestObjective){
-                    bestObjective = objective;
-                    bestMerge = new int[] {n1.getId(), n2.getId()};
-                    if (objective == 1.0){
-                        return bestMerge;
-                    }
-                }
-                n1.getContainedNodes().addAll(containedNodesBackup1);
-                n2.getContainedNodes().addAll(containedNodesBackup2);
-
-                summary.nodeWithId(Integer.MAX_VALUE).getContainedNodes().clear();
-                List<BaseEdge> edges = new ArrayList<>(summary.outEdgesFor(Integer.MAX_VALUE));
-                edges.addAll(summary.inEdgesFor(Integer.MAX_VALUE));
-                for (BaseEdge e: edges){
-                    summary.removeEdge(e);
-                }
-
-            }
-        }
-        return bestMerge;
     }
 }
