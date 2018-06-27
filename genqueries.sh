32496d1f5c83a8e7789e59df3360027b93ed7156
Lukas <lukas.faber@student.hpi.uni-potsdam.de>
2018-06-27 19:11:31 +0200

    add  script for query generation

---
+++ genqueries.sh
@@ -0,0 +1,11 @@
+java -Xmx14G -jar target/adaptive_summarization-1.0-SNAPSHOT.jar ~/thesisdata/citation/graph_3 ~/thesisdata/citation/queries2/   2 2 100 0.03
+
+java -Xmx14G -jar target/adaptive_summarization-1.0-SNAPSHOT.jar ~/thesisdata/citation/graph_3 ~/thesisdata/citation/queries3/   3 3 100 0.03
+
+java -Xmx14G -jar target/adaptive_summarization-1.0-SNAPSHOT.jar ~/thesisdata/citation/graph_3 ~/thesisdata/citation/queries4/   4 4 100 0.03
+
+java -Xmx14G -jar target/adaptive_summarization-1.0-SNAPSHOT.jar ~/thesisdata/citation/graph_3 ~/thesisdata/citation/queries5/   5 5 100 0.03
+
+java -Xmx14G -jar target/adaptive_summarization-1.0-SNAPSHOT.jar ~/thesisdata/citation/graph_3 ~/thesisdata/citation/queries6/   6 6 100 0.03
+
+java -Xmx14G -jar target/adaptive_summarization-1.0-SNAPSHOT.jar ~/thesisdata/citation/graph_3 ~/thesisdata/citation/queries7/   7 7 100 0.03
