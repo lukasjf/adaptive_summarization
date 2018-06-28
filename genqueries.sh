ebccf266a3b21a94729b8ea81c45320a79a8f310
Lukas <lukas.j.faber@gmail.com>
2018-06-28 09:53:23 +0200

    query geeneration with varying randomness

--- genqueries.sh
+++ genqueries.sh
@@ -1,11 +1,21 @@
-java -Xmx14G -jar target/adaptive_summarization-1.0-SNAPSHOT.jar ~/thesisdata/citation/graph_3 ~/thesisdata/citation/queries2/   2 2 100 0.03
+#java -Xmx6G -jar target/adaptive_summarization-1.0-SNAPSHOT.jar $1 $2"query2/" 2 2 100 0.03
 
-java -Xmx14G -jar target/adaptive_summarization-1.0-SNAPSHOT.jar ~/thesisdata/citation/graph_3 ~/thesisdata/citation/queries3/   3 3 100 0.03
+#java -Xmx6G -jar target/adaptive_summarization-1.0-SNAPSHOT.jar $1 $2"query3/" 3 3 100 0.03
 
-java -Xmx14G -jar target/adaptive_summarization-1.0-SNAPSHOT.jar ~/thesisdata/citation/graph_3 ~/thesisdata/citation/queries4/   4 4 100 0.03
+#java -Xmx6G -jar target/adaptive_summarization-1.0-SNAPSHOT.jar $1 $2"query4/" 4 4 100 0.03
 
-java -Xmx14G -jar target/adaptive_summarization-1.0-SNAPSHOT.jar ~/thesisdata/citation/graph_3 ~/thesisdata/citation/queries5/   5 5 100 0.03
+#java -Xmx6G -jar target/adaptive_summarization-1.0-SNAPSHOT.jar $1 $2"query5/" 5 5 100 0.03
 
-java -Xmx14G -jar target/adaptive_summarization-1.0-SNAPSHOT.jar ~/thesisdata/citation/graph_3 ~/thesisdata/citation/queries6/   6 6 100 0.03
+#java -Xmx6G -jar target/adaptive_summarization-1.0-SNAPSHOT.jar $1 $2"query6/" 6 6 100 0.03
 
-java -Xmx14G -jar target/adaptive_summarization-1.0-SNAPSHOT.jar ~/thesisdata/citation/graph_3 ~/thesisdata/citation/queries7/   7 7 100 0.03
+#java -Xmx6G -jar target/adaptive_summarization-1.0-SNAPSHOT.jar $1 $2"/query7/" 7 7 100 0.03
+
+java -Xmx6G -jar target/adaptive_summarization-1.0-SNAPSHOT.jar $1 $2"query1/" 2 4 30 1.0
+
+java -Xmx6G -jar target/adaptive_summarization-1.0-SNAPSHOT.jar $1 $2"query033/" 2 4 30 0.33
+
+java -Xmx6G -jar target/adaptive_summarization-1.0-SNAPSHOT.jar $1 $2"query01/" 2 4 30 0.1
+
+java -Xmx6G -jar target/adaptive_summarization-1.0-SNAPSHOT.jar $1 $2"query0033/" 2 4 30 0.33
+
+java -Xmx6G -jar target/adaptive_summarization-1.0-SNAPSHOT.jar $1 $2"query001/" 2 4 30 0.01
