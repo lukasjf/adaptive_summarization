dd2ac2c3e41b6a9a4ccd6181684710d47de79ba8
Lukas <lukas.j.faber@gmail.com>
2018-09-25 17:09:42 +0200

    add dendensification compression

---
+++ src/main/java/main/DedensificationMain.java
@@ -0,0 +1,7 @@
+package main;
+
+/**
+ * Created by lukas on 25.09.18.
+ */
+public class DedensificationMain {
+}
---
+++ src/main/java/summary/dedensification/DedensifiedSummary.java
@@ -0,0 +1,7 @@
+package summary.dedensification;
+
+/**
+ * Created by lukas on 25.09.18.
+ */
+public class DedensifiedSummary {
+}
