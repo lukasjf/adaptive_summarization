04041fa668b62724b94a542f70daf567f2b01caa
Lukas <lukas.j.faber@gmail.com>
2018-09-26 17:37:51 +0200

    fix to DedensificationSummary

--- src/main/java/summary/dedensification/DedensifiedSummary.java
+++ src/main/java/summary/dedensification/DedensifiedSummary.java
@@ -14,9 +14,9 @@ public class DedensifiedSummary implements Benchmarkable{
 
     private BaseGraph original;
     private BaseGraph summary;
+    int counter = Integer.MAX_VALUE;
 
     public DedensifiedSummary(BaseGraph graph, int tau) {
-        int counter = Integer.MAX_VALUE;
         original = graph;
         long graphSize = new GraphEncoder().encode(graph);
         summary = new BaseGraph();
@@ -26,55 +26,105 @@ public class DedensifiedSummary implements Benchmarkable{
         }
         for (String edgelabel : graph.getEdges().stream().map(BaseEdge::getLabel).collect(Collectors.toSet())) {
 
-            //System.out.println(edgelabel);
+            doInHigh(tau, edgelabel);
+            //doOutHigh(tau, edgelabel);
+        }
+        System.out.println("" + tau + "," + graphSize + "," + new GraphEncoder().encode(summary));
+    }
 
-            Set<Integer> high_degree = new HashSet<>();
+    private void doInHigh(int tau, String edgelabel) {
+        Set<Integer> high_degree = new HashSet<>();
 
-            for (BaseNode n : graph.getNodes()) {
-                if (graph.outEdgesFor(n.getId()).stream().filter(e -> e.getLabel().equals(edgelabel)).count() >= tau) {
-                    high_degree.add(n.getId());
-                }
+        //A node is high degree with at least tau incoming edges of a label
+        for (BaseNode n : original.getNodes()) {
+            if (original.inEdgesFor(n.getId()).stream().filter(e -> e.getLabel().equals(edgelabel)).count() >= tau) {
+                high_degree.add(n.getId());
             }
+        }
 
-            Map<Set<Integer>, Set<Integer>> w = new HashMap<>();
-            for (BaseNode n : graph.getNodes()) {
-                Set<Integer> high_connected = graph.outEdgesFor(n.getId()).stream()
-                        .filter(e -> e.getLabel().equals(edgelabel) && high_degree.contains(e.getTarget().getId()))
-                        .map(e -> e.getTarget().getId()).collect(Collectors.toSet());
-                if (!w.containsKey(high_connected)) {
-                    w.put(high_connected, new HashSet<>());
-                }
-                w.get(high_connected).add(n.getId());
+        // group all nodes depending on which high-degree nodes they are connected too
+        Map<Set<Integer>, Set<Integer>> w = new HashMap<>();
+        for (BaseNode n : original.getNodes()) {
+            Set<Integer> high_connected = original.outEdgesFor(n.getId()).stream()
+                    .filter(e -> e.getLabel().equals(edgelabel) && high_degree.contains(e.getTarget().getId()))
+                    .map(e -> e.getTarget().getId()).collect(Collectors.toSet());
+            if (!w.containsKey(high_connected)) {
+                w.put(high_connected, new HashSet<>());
             }
+            w.get(high_connected).add(n.getId());
+        }
 
-            for (Set<Integer> key : w.keySet()) {
-                if (key.isEmpty() || notWorthIt(key.size(), w.get(key).size())) {
-                    for (int n : w.get(key)) {
-                        for (BaseEdge e : graph.outEdgesFor(n)) {
-                            if (e.getLabel().equals(edgelabel)) {
-                                summary.addEdge(n, e.getTarget().getId(), edgelabel);
-                            }
+        // add a compressor node, if we can save storage --- otherwise add edges directly
+        for (Set<Integer> key : w.keySet()) {
+            if (key.isEmpty() || notWorthIt(key.size(), w.get(key).size())) {
+                for (int n : w.get(key)) {
+                    for (BaseEdge e : original.outEdgesFor(n)) {
+                        if (e.getLabel().equals(edgelabel)) {
+                            summary.addEdge(n, e.getTarget().getId(), edgelabel);
                         }
                     }
-                } else {
-                    BaseNode dedens = summary.addNode(counter--, "");
-                    for (int n : key) {
-                        summary.addEdge(dedens.getId(), n, edgelabel);
-                    }
-                    for (int n : w.get(key)) {
-                        summary.addEdge(n, dedens.getId(), edgelabel);
-                        for (BaseEdge e : graph.outEdgesFor(n)) {
-                            if (e.getLabel().equals(edgelabel) && !key.contains(e.getTarget().getId())) {
-                                summary.addEdge(n, e.getTarget().getId(), edgelabel);
-                            } else {
-                                int i = 0;
-                            }
+                }
+            } else {
+                BaseNode dedens = summary.addNode(counter--, "");
+                for (int n : key) {
+                    summary.addEdge(dedens.getId(), n, edgelabel);
+                }
+                for (int n : w.get(key)) {
+                    summary.addEdge(n, dedens.getId(), edgelabel);
+                    for (BaseEdge e : original.outEdgesFor(n)) {
+                        if (e.getLabel().equals(edgelabel) && !key.contains(e.getTarget().getId())) {
+                            summary.addEdge(n, e.getTarget().getId(), edgelabel);
                         }
                     }
                 }
             }
         }
-        System.out.println("" + tau + "," + graphSize + "," + new GraphEncoder().encode(summary));
+    }
+
+    private void doOutHigh(int tau, String edgelabel) {
+        Set<Integer> high_degree = new HashSet<>();
+
+        for (BaseNode n : original.getNodes()) {
+            if (original.outEdgesFor(n.getId()).stream().filter(e -> e.getLabel().equals(edgelabel)).count() >= tau) {
+                high_degree.add(n.getId());
+            }
+        }
+
+        Map<Set<Integer>, Set<Integer>> w = new HashMap<>();
+        for (BaseNode n : original.getNodes()) {
+            Set<Integer> high_connected = original.inEdgesFor(n.getId()).stream()
+                    .filter(e -> e.getLabel().equals(edgelabel) && high_degree.contains(e.getSource().getId()))
+                    .map(e -> e.getSource().getId()).collect(Collectors.toSet());
+            if (!w.containsKey(high_connected)) {
+                w.put(high_connected, new HashSet<>());
+            }
+            w.get(high_connected).add(n.getId());
+        }
+
+        for (Set<Integer> key : w.keySet()) {
+            if (key.isEmpty() || notWorthIt(key.size(), w.get(key).size())) {
+                for (int n : w.get(key)) {
+                    for (BaseEdge e : original.inEdgesFor(n)) {
+                        if (e.getLabel().equals(edgelabel)) {
+                            summary.addEdge(e.getSource().getId(), n, edgelabel);
+                        }
+                    }
+                }
+            } else {
+                BaseNode dedens = summary.addNode(counter--, "");
+                for (int n : key) {
+                    summary.addEdge(n, dedens.getId(), edgelabel);
+                }
+                for (int n : w.get(key)) {
+                    summary.addEdge(dedens.getId(), n, edgelabel);
+                    for (BaseEdge e : original.inEdgesFor(n)) {
+                        if (e.getLabel().equals(edgelabel) && !key.contains(e.getSource().getId())) {
+                            summary.addEdge(e.getSource().getId(), n, edgelabel);
+                        }
+                    }
+                }
+            }
+        }
     }
 
     private boolean notWorthIt(int size1, int size2) {
